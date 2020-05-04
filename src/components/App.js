210c5189936f238e80977da91dca69b1f59dc02a
Meghan Martin <meghanxmartin@gmail.com>
2020-05-04 19:54:11 +0200

    Adds font

--- src/components/App.js
+++ src/components/App.js
@@ -13,7 +13,12 @@ function App() {
         <Header />
         </div>
       </div>
-      <iframe width="100%" height="60" src="https://www.mixcloud.com/widget/iframe/?hide_cover=1&mini=1&feed=%2FNTSRadio%2F" frameborder="0" ></iframe>
+      <iframe
+        width="100%"
+        height="60"
+        src="https://www.mixcloud.com/widget/iframe/?hide_cover=1&mini=1&feed=%2FNTSRadio%2F"
+        frameBorder="0" >
+        />
     </div>
   );
 }

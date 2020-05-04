ad1ce683b04eb1497a8b594538cabfaec5d744b1
Meghan Martin <meghanxmartin@gmail.com>
2020-05-04 19:44:28 +0200

    creates featured mix component

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,18 @@
 import React from 'react';
+import FeaturedMix from './FeaturedMix';
 
 function App() {
   return (
     <div className="App">
+      <div>
+        <div>
+        <FeaturedMix />
+
+        </div>
+        <div>
+
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/components/FeaturedMix.js
@@ -0,0 +1,10 @@
+import React, {Component} from 'react';
+
+const FeaturedMix = props => (
+  <div>
+    <h1>Featured Mix </h1>
+    <h1> example song </h1>
+  </div>
+);
+
+export default FeaturedMix

ddfbad50b0fb79d1c1752eda252fcecb681451bc
Meghan Martin <meghanxmartin@gmail.com>
2020-05-04 19:49:48 +0200

    adds music player

--- src/components/App.js
+++ src/components/App.js
@@ -13,6 +13,7 @@ function App() {
         <Header />
         </div>
       </div>
+      <iframe width="100%" height="60" src="https://www.mixcloud.com/widget/iframe/?hide_cover=1&mini=1&feed=%2FNTSRadio%2F" frameborder="0" ></iframe>
     </div>
   );
 }
